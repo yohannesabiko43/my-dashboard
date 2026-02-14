430d2b8f844e1c29ba66e62fd21be5be872a70ce
Yohannes Ashango <yohannesmood6@gmail.com>
2026-02-14 10:31:57 +0300

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+        
+
+    return (
+        <div className="bro">
+            <img src="src\photo_2025-11-25_14-47-18.jpg" alt="" />
+            <h2>Bro Code</h2>
+            <p>i make youtube video and play video games</p>
+        </div>
+
+
+    );
+    }
+export default Card;
